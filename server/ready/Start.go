43e67b2fc7f2d65188de5970dd49b497921489fa
墨七 <meichangliang@mo7.cc>
2022-07-23 18:07:52 +0800

    exit-push

--- server/ready/Start.go
+++ server/ready/Start.go
@@ -3,8 +3,12 @@ package ready
 import (
 	"time"
 
+	"CoinMarket.net/server/global/config"
+	"CoinMarket.net/server/okxApi/okxInfo"
 	"CoinMarket.net/server/okxApi/restApi/inst"
 	"github.com/EasyGolang/goTools/mCycle"
+	"github.com/EasyGolang/goTools/mFile"
+	"github.com/EasyGolang/goTools/mJson"
 )
 
 func Start() {
@@ -12,4 +16,7 @@ func Start() {
 		Func:      inst.Start,
 		SleepTime: time.Hour * 4, // 每 4 时获取一次
 	}).Start()
+
+	mFile.Write(config.Dir.JsonData+"/SPOT_inst.json", mJson.ToStr(okxInfo.SPOT_inst))
+	mFile.Write(config.Dir.JsonData+"/SWAP_inst.json", mJson.ToStr(okxInfo.SWAP_inst))
 }
