1fd3071c31a1386ed2f6ba900849bd3cf3eca07b
墨七 <meichangliang@mo7.cc>
2022-07-23 19:23:51 +0800

    exit-push

--- server/okxApi/restApi/tickers/Start.go
+++ server/okxApi/restApi/tickers/Start.go
@@ -2,4 +2,5 @@ package tickers
 
 // 获取行情信息
 func Start() {
+	GetTicker()
 }
--- server/ready/Start.go
+++ server/ready/Start.go
@@ -6,6 +6,7 @@ import (
 	"CoinMarket.net/server/global/config"
 	"CoinMarket.net/server/okxApi/okxInfo"
 	"CoinMarket.net/server/okxApi/restApi/inst"
+	"CoinMarket.net/server/okxApi/restApi/tickers"
 	"github.com/EasyGolang/goTools/mCycle"
 	"github.com/EasyGolang/goTools/mFile"
 	"github.com/EasyGolang/goTools/mJson"
@@ -15,8 +16,13 @@ func Start() {
 	// 获取 OKX 交易产品信息
 	mCycle.New(mCycle.Opt{
 		Func:      inst.Start,
-		SleepTime: time.Hour * 4, // 每 4 时获取一次
+		SleepTime: time.Hour * 8, // 每 8 时获取一次
 	}).Start()
 	mFile.Write(config.Dir.JsonData+"/SPOT_inst.json", mJson.ToStr(okxInfo.SPOT_inst))
 	mFile.Write(config.Dir.JsonData+"/SWAP_inst.json", mJson.ToStr(okxInfo.SWAP_inst))
+
+	mCycle.New(mCycle.Opt{
+		Func:      tickers.Start,
+		SleepTime: time.Minute, // 每 1 分钟 获取一次
+	}).Start()
 }
