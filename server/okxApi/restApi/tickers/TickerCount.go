7c2a459527fc7d2009bc038f7b425a3ba5403432
墨七 <meichangliang@mo7.cc>
2022-07-26 05:38:15 +0800

    exit-push

--- server/okxApi/restApi/tickers/TickerCount.go
+++ server/okxApi/restApi/tickers/TickerCount.go
@@ -23,8 +23,8 @@ func TickerCount(data okxInfo.TickerType, BinanceTicker okxInfo.BinanceTickerTyp
 	// 成交量总和
 	Ticker.Volume = mCount.Add(Ticker.QuoteVolume, Ticker.VolCcy24H)
 
-	Ticker.BinanceVolRose = mCount.RoseCent(Ticker.QuoteVolume, Ticker.Volume)
-	Ticker.OkxVolRose = mCount.RoseCent(Ticker.VolCcy24H, Ticker.Volume)
+	Ticker.BinanceVolRose = mCount.PerCent(Ticker.QuoteVolume, Ticker.Volume)
+	Ticker.OkxVolRose = mCount.PerCent(Ticker.VolCcy24H, Ticker.Volume)
 
 	return Ticker
 }
