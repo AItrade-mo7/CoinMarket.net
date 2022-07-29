5dac1bce7bf3cc55f6b7fcf1eb5d61f236ad3cad
墨七 <meichangliang@mo7.cc>
2022-07-30 06:02:46 +0800

    exit-push

--- server/tickerAnalyse/WholeAnalyse.go
+++ server/tickerAnalyse/WholeAnalyse.go
@@ -5,7 +5,6 @@ import (
 
 	"CoinMarket.net/server/okxApi/okxInfo"
 	"github.com/EasyGolang/goTools/mCount"
-	"github.com/EasyGolang/goTools/mJson"
 	"github.com/EasyGolang/goTools/mStr"
 	"github.com/EasyGolang/goTools/mTime"
 )
@@ -89,8 +88,6 @@ func WholeAnalyse() (resData WholeResultType) {
 
 	resData.MaxUP = okxInfo.TickerU_R24[0]
 	resData.MaxDown = okxInfo.TickerU_R24[len(okxInfo.TickerU_R24)-1]
-
-	mJson.Println(resData)
 
 	return resData
 }
