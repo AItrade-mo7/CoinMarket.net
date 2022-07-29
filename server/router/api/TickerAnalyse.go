82b0318e862dc49dd396db2163a015348d258759
墨七 <meichangliang@mo7.cc>
2022-07-30 06:14:23 +0800

    exit-push

--- server/router/api/TickerAnalyse.go
+++ server/router/api/TickerAnalyse.go
@@ -1,10 +1,11 @@
 package api
 
 import (
+	"CoinMarket.net/server/okxInfo"
 	"CoinMarket.net/server/router/result"
 	"github.com/gofiber/fiber/v2"
 )
 
 func TickerAnalyse(c *fiber.Ctx) error {
-	return c.JSON(result.Succeed.WithData("学习"))
+	return c.JSON(result.Succeed.WithData(okxInfo.WholeTickerAnalyse))
 }
