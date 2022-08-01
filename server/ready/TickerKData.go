806c9c55c149b8930c8cacc61c6b15b7c19f188f
墨七 <meichangliang@mo7.cc>
2022-08-01 22:09:10 +0800

    测试模式

--- server/ready/TickerKData.go
+++ server/ready/TickerKData.go
@@ -3,12 +3,20 @@ package ready
 import (
 	"time"
 
+	"CoinMarket.net/server/global/config"
 	"CoinMarket.net/server/okxApi/restApi/kdata"
 	"CoinMarket.net/server/okxInfo"
 	"CoinMarket.net/server/tickerAnalyse"
 )
 
 func TickerKdata() {
+	if config.AppEnv.RunMod == 1 {
+		item := okxInfo.TickerList[0]
+		list := kdata.GetKdata(item.InstID)
+		tickerAnalyse.NewSingle(list)
+		return
+	}
+
 	for _, item := range okxInfo.TickerList {
 		time.Sleep(time.Second / 5) // 1秒最多 5 次
 		list := kdata.GetKdata(item.InstID)
