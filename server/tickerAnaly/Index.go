47a663e6ae888762fbd07b9dfb56b753e81e3279
墨七 <meichangliang@mo7.cc>
2022-08-08 07:37:27 +0800

    exit-push

--- server/tickerAnaly/Index.go
+++ server/tickerAnaly/Index.go
@@ -7,11 +7,14 @@ import (
 
 func Start() {
 	// 基于  mOKX.MarketKdata  进行数据分析
-	okxInfo.TickerAnalySingle = make(map[string][]mOKX.AnalySliceType)
+	TickerAnalySingle := make(map[string][]mOKX.AnalySliceType)
 	for _, list := range okxInfo.MarketKdata {
 		Single := NewSingle(list)
-		okxInfo.TickerAnalySingle[Single.Info.InstID] = Single.ResData
+		TickerAnalySingle[Single.Info.InstID] = Single.ResData
 	}
+
+	okxInfo.TickerAnalySingle = make(map[string][]mOKX.AnalySliceType)
+	okxInfo.TickerAnalySingle = TickerAnalySingle
 
 	WholeAnaly()
 }
