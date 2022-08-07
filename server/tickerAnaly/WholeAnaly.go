76635ba986ff73b8f204f3017d2fff8fb05620fe
墨七 <meichangliang@mo7.cc>
2022-08-07 08:44:06 +0800

    exit-push

--- server/tickerAnaly/WholeAnaly.go
+++ server/tickerAnaly/WholeAnaly.go
@@ -14,52 +14,61 @@ import (
 */
 
 func WholeAnaly() {
+	TickerAnalyWhole := []mOKX.TypeWholeTickerAnaly{}
+
+	okxInfo.TickerAnalyWhole = []mOKX.TypeWholeTickerAnaly{}
+
 	TickerSingle := make(map[int][]mOKX.AnalySliceType)
 
 	TickerVolumeList := make(map[int][]mOKX.AnalySliceType)
-	TickerRoseList := make(map[int][]mOKX.AnalySliceType)
+
+	/*
+		币种时间切片
+		okxInfo.TickerAnalySingle
+	*/
 
 	for _, Slice := range okxInfo.TickerAnalySingle {
 		for _, Single := range Slice {
 			TickerSingle[Single.DiffHour] = append(TickerSingle[Single.DiffHour], Single)
 		}
 	}
-	for key, list := range TickerSingle {
-		if key == 24 {
-			TickerVolumeList[key] = mOKX.SortAnalySlice_Volume(list)
-			TickerRoseList[key] = mOKX.SortAnalySlice_UR(list)
 
-			TickerWholeAnaly(list)
-		}
+	for key, list := range TickerSingle {
+		TickerVolumeList[key] = mOKX.SortAnalySlice_Volume(list)
 	}
+	for _, list := range TickerVolumeList {
+		Analy := TickerWholeAnaly(list)
+		TickerAnalyWhole = append(TickerAnalyWhole, Analy)
+	}
+
+	okxInfo.TickerAnalyWhole = mOKX.Sort_DiffHour(TickerAnalyWhole)
 }
 
 func TickerWholeAnaly(list []mOKX.AnalySliceType) (resData mOKX.TypeWholeTickerAnaly) {
 	resData = mOKX.TypeWholeTickerAnaly{}
 
-	if len(okxInfo.TickerList) < 3 {
+	if len(list) != len(okxInfo.TickerList) {
 		return
 	}
 
 	// 开始
-
 	var (
-		Up_Num   []string
-		Down_Num []string
+		Up_Num   []string // 上涨幅度的集合
+		Down_Num []string // 下跌幅度的集合
 	)
 
-	for _, val := range okxInfo.TickerList {
-		U_24_diff := mCount.Le(val.U_R24, "0")
-		if U_24_diff > -1 {
-			Up_Num = append(Up_Num, val.U_R24)
+	for _, val := range list {
+		U_R_diff := mCount.Le(val.RosePer, "0")
+		if U_R_diff > -1 {
+			Up_Num = append(Up_Num, val.RosePer)
 		} else {
-			Down_Num = append(Down_Num, val.U_R24)
+			Down_Num = append(Down_Num, val.RosePer)
 		}
 	}
 
 	// 上涨指数
 	upN := mStr.ToStr(len(Up_Num))
-	allN := mStr.ToStr(len(okxInfo.TickerList))
+	allN := mStr.ToStr(len(list))
 
 	resData.UPIndex = mCount.PerCent(upN, allN)
 
@@ -81,10 +90,11 @@ func TickerWholeAnaly(list []mOKX.AnalySliceType) (resData mOKX.TypeWholeTickerA
 		resData.DirIndex = -1
 	}
 
-	resData.Ts = okxInfo.TickerList[0].Ts
+	resData.Ts = list[0].EndTimeUnix
 
-	resData.MaxUP = okxInfo.TickerU_R24[0]
-	resData.MaxDown = okxInfo.TickerU_R24[len(okxInfo.TickerU_R24)-1]
+	resData.MaxUP = list[0]
+	resData.MaxDown = list[len(list)-1]
+	resData.DiffHour = list[0].DiffHour
 
 	return resData
 }
