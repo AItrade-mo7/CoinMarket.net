8a11408fca675318e52bb3c5b6f111e65739eb56
墨七 <meichangliang@mo7.cc>
2022-08-07 23:16:43 +0800

    exit-push

--- server/router/api/Tickers.go
+++ server/router/api/Tickers.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"CoinMarket.net/server/global/config"
 	"CoinMarket.net/server/okxInfo"
 	"CoinMarket.net/server/router/result"
 	"github.com/EasyGolang/goTools/mFiber"
@@ -16,6 +17,7 @@ type TickerResType struct {
 	List        []mOKX.TypeTicker                `json:"List"`        // 列表
 	AnalyWhole  []mOKX.TypeWholeTickerAnaly      `json:"AnalyWhole"`  // 大盘分析结果
 	AnalySingle map[string][]mOKX.AnalySliceType `json:"AnalySingle"` // 单个币种分析结果
+	Unit        string
 }
 
 func Tickers(c *fiber.Ctx) error {
@@ -26,6 +28,7 @@ func Tickers(c *fiber.Ctx) error {
 	TickerRes.List = okxInfo.TickerList
 	TickerRes.AnalyWhole = okxInfo.TickerAnalyWhole
 	TickerRes.AnalySingle = okxInfo.TickerAnalySingle
+	TickerRes.Unit = config.Unit
 
 	if json.SortType == "U_R24" {
 		TickerRes.List = okxInfo.TickerU_R24
