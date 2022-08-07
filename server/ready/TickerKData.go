e9b5997df4851088eb77f9e293e2915c7c7f4636
墨七 <meichangliang@mo7.cc>
2022-08-08 05:20:06 +0800

    exit-push

--- server/ready/TickerKData.go
+++ server/ready/TickerKData.go
@@ -13,7 +13,7 @@ import (
 func TickerKdata() {
 	okxInfo.MarketKdata = make(map[string][]mOKX.TypeKd)
 	for _, item := range okxInfo.TickerList {
-		time.Sleep(time.Second / 5) // 1秒最多 5 次
+		time.Sleep(time.Second) // 1秒最多 1 次
 		List := DataMerge(DataMergeOpt{
 			OKXList:     kdata.GetKdata(item.InstID),
 			BinanceList: binanceApi.GetKdata(item.Symbol),
