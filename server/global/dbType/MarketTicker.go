20d95abc8e307a3859eac72921bfc92cce7f1aee
墨七 <meichangliang@mo7.cc>
2022-09-08 03:16:24 +0800

    exit-push

--- server/global/dbType/MarketTicker.go
+++ server/global/dbType/MarketTicker.go
@@ -9,14 +9,16 @@ import (
 )
 
 type MarketTickerTable struct {
-	List        []mOKX.TypeTicker                `bson:"List"`        // 成交量排序列表
-	ListU_R24   []mOKX.TypeTicker                `bson:"ListU_R24"`   // 涨跌幅排序列表
-	AnalyWhole  []mOKX.TypeWholeTickerAnaly      `bson:"AnalyWhole"`  // 大盘分析结果
-	AnalySingle map[string][]mOKX.AnalySliceType `bson:"AnalySingle"` // 单个币种分析结果
-	Unit        string                           `bson:"Unit"`
-	WholeDir    int                              `bson:"WholeDir"`
-	TimeUnix    int64                            `bson:"TimeUnix"`
-	Date        string                           `bson:"Date"`
+	List           []mOKX.TypeTicker                `bson:"List"`        // 成交量排序列表
+	ListU_R24      []mOKX.TypeTicker                `bson:"ListU_R24"`   // 涨跌幅排序列表
+	AnalyWhole     []mOKX.TypeWholeTickerAnaly      `bson:"AnalyWhole"`  // 大盘分析结果
+	AnalySingle    map[string][]mOKX.AnalySliceType `bson:"AnalySingle"` // 单个币种分析结果
+	Unit           string                           `bson:"Unit"`
+	WholeDir       int                              `bson:"WholeDir"`
+	TimeUnix       int64                            `bson:"TimeUnix"`
+	Time           string                           `bson:"Time"`
+	CreateTimeUnix int64                            `bson:"CreateTimeUnix"`
+	CreateTime     string                           `bson:"CreateTime"`
 }
 
 // 拼接数据
@@ -29,9 +31,10 @@ func GetTickerDB() MarketTickerTable {
 	TickerRes.Unit = config.Unit
 	TickerRes.WholeDir = okxInfo.WholeDir
 	TickerRes.TimeUnix = okxInfo.TickerList[0].Ts
+	TickerRes.Time = mTime.UnixFormat(mStr.ToStr(TickerRes.TimeUnix))
 
-	TickerDate := mTime.UnixFormat(mStr.ToStr(TickerRes.TimeUnix))
-	TickerRes.Date = TickerDate
+	TickerRes.CreateTimeUnix = mTime.GetUnixInt64()
+	TickerRes.CreateTime = mTime.UnixFormat(mStr.ToStr(TickerRes.CreateTime))
 
 	return TickerRes
 }
