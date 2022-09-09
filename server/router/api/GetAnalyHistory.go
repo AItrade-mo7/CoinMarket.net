ba8a1a7138f50a57b2897a3535ad33d783547fb3
墨七 <meichangliang@mo7.cc>
2022-09-09 10:50:13 +0800

    exit-push

--- server/router/api/GetAnalyHistory.go
+++ server/router/api/GetAnalyHistory.go
@@ -6,13 +6,21 @@ import (
 	"CoinMarket.net/server/global/config"
 	"CoinMarket.net/server/global/dbType"
 	"CoinMarket.net/server/router/result"
+	"github.com/EasyGolang/goTools/mFiber"
 	"github.com/EasyGolang/goTools/mMongo"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type GetAnalyHistoryParam struct {
+	Current int64 `json:"Current"` // 列表
+}
+
 func GetAnalyHistory(c *fiber.Ctx) error {
+	var json GetAnalyHistoryParam
+	mFiber.Parser(c, &json)
+
 	db := mMongo.New(mMongo.Opt{
 		UserName: config.SysEnv.MongoUserName,
 		Password: config.SysEnv.MongoPassword,
@@ -24,11 +32,12 @@ func GetAnalyHistory(c *fiber.Ctx) error {
 	FK := bson.D{}
 	findOpt := options.Find()
 
-	findOpt.SetLimit(1400)
+	findOpt.SetLimit(300)
 	findOpt.SetSort(bson.D{{
 		Key:   "TimeUnix",
 		Value: -1,
 	}})
+	findOpt.SetSkip(json.Current * 300)
 
 	cur, err := db.Table.Find(db.Ctx, FK, findOpt)
 	if err != nil {
