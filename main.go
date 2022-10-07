a949d475282762bee4691efd28c91d0a22429616
墨七 <meichangliang@mo7.cc>
2022-10-07 10:46:19 +0800

    exit-push

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
 
 	"CoinMarket.net/server/global"
 	"CoinMarket.net/server/global/config"
 	"CoinMarket.net/server/ready"
 	"CoinMarket.net/server/router"
-	"github.com/EasyGolang/goTools/mMongo"
-	"github.com/EasyGolang/goTools/mOKX"
 	jsoniter "github.com/json-iterator/go"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 //go:embed package.json
@@ -26,29 +22,4 @@ func main() {
 	ready.Start()
 
 	router.Start()
-
-	// OrganizeDatabase()
-}
-
-func OrganizeDatabase() {
-	fmt.Println("整理数据库")
-	db := mMongo.New(mMongo.Opt{
-		UserName: config.SysEnv.MongoUserName,
-		Password: config.SysEnv.MongoPassword,
-		Address:  config.SysEnv.MongoAddress,
-		DBName:   "AITrade",
-	}).Connect().Collection("BTCUSDT")
-	defer db.Close()
-	cur, err := db.Table.Find(db.Ctx, bson.D{})
-	if err != nil {
-		fmt.Println("数据库错误")
-		return
-	}
-
-	for cur.Next(db.Ctx) {
-		var result mOKX.TypeKd
-		cur.Decode(&result)
-
-		fmt.Println(result.TimeStr)
-	}
 }
