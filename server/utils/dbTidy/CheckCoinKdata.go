e549862e558b593bb4384ae875ff84f4b8d5026a
墨七 <meichangliang@mo7.cc>
2022-10-12 04:12:46 +0800

    exit-push

--- server/utils/dbTidy/CheckCoinKdata.go
+++ server/utils/dbTidy/CheckCoinKdata.go
@@ -5,7 +5,12 @@ import (
 
 	"CoinMarket.net/server/global"
 	"CoinMarket.net/server/global/config"
+	"github.com/EasyGolang/goTools/mJson"
 	"github.com/EasyGolang/goTools/mMongo"
+	"github.com/EasyGolang/goTools/mOKX"
+	jsoniter "github.com/json-iterator/go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func CheckCoinKdata() {
@@ -20,4 +25,25 @@ func CheckCoinKdata() {
 	}).Connect().Collection(CoinName + "USDT")
 	defer global.Run.Println("关闭数据库连接" + CoinName)
 	defer db.Close()
+
+	findOpt := options.Find()
+	findOpt.SetAllowDiskUse(true)
+	findOpt.SetSort(map[string]int{
+		"TimeUnix": 1,
+	})
+	FK := bson.D{}
+	cursor, err := db.Table.Find(db.Ctx, FK, findOpt)
+
+	for cursor.Next(db.Ctx) {
+		var curData map[string]any
+		cursor.Decode(&curData)
+		var Kdata mOKX.TypeKd
+		jsoniter.Unmarshal(mJson.ToJson(curData), &Kdata)
+
+		global.Run.Println("==结束==", Kdata.InstID, Kdata.TimeStr, Kdata.C)
+	}
+
+	if err != nil {
+		global.LogErr(err)
+	}
 }
