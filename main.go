7c6b96b14955674dc347c3714fc4ff3c57301c59
墨七 <meichangliang@mo7.cc>
2022-10-22 18:59:14 +0800

    exit-push

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 
 	"CoinMarket.net/server/global"
 	"CoinMarket.net/server/global/config"
-	"CoinMarket.net/server/utils/dbTask"
+	"CoinMarket.net/server/ready"
+	"CoinMarket.net/server/router"
 	jsoniter "github.com/json-iterator/go"
 )
 
@@ -18,12 +19,12 @@ func main() {
 	// 初始化系统参数
 	global.Start()
 
-	// ready.Start()
-	// router.Start()
+	ready.Start()
+	router.Start()
 
 	// ==== 开始整理算法结果 ====
-	Task := dbTask.NewAnalyTask()
-	Task.CoinDBTraverse()
+	// Task := dbTask.NewAnalyTask()
+	// Task.CoinDBTraverse()
 
 	// ==== 开始填充榜单历史 ====
 	// FormatDB := dbTask.NewFormat()
