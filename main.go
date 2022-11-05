925155f589872cd5b7d35380f59091f438c8d8a4
墨七 <meichangliang@mo7.cc>
2022-11-06 02:54:35 +0800

    exit-push

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"CoinMarket.net/server/global"
 	"CoinMarket.net/server/global/config"
 	"CoinMarket.net/server/ready"
+	"CoinMarket.net/server/router"
 	jsoniter "github.com/json-iterator/go"
 )
 
@@ -20,7 +21,7 @@ func main() {
 
 	ready.Start()
 
-	// router.Start()
+	router.Start()
 
 	// ==== 开始整理算法结果 ====
 	// Task := dbTask.NewAnalyTask()
