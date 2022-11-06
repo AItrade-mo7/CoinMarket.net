b630664cd3b2395e0938e3beaf82afe13dced6c7
墨七 <meichangliang@mo7.cc>
2022-11-07 04:58:37 +0800

    exit-push

--- server/ready/Start.go
+++ server/ready/Start.go
@@ -31,7 +31,7 @@ func Start() {
 	SetTickerAnaly() // 默认执行一次
 	go mClock.New(mClock.OptType{
 		Func: SetTickerAnaly,
-		Spec: "59 0,5,10,15,20,25,30,35,40,45,50,55 * * * ? ", // 5 分的整数过 59 秒
+		Spec: "20 0,5,10,15,20,25,30,35,40,45,50,55 * * * ? ", // 5 分的整数过 10 秒
 	})
 }
 
