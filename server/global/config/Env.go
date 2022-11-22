c7501e18693c4f49122a1cc3b32c29d617744e09
墨七 <meichangliang@mo7.cc>
2022-11-22 23:17:49 +0800

    exit-push

--- server/global/config/Env.go
+++ server/global/config/Env.go
@@ -19,6 +19,14 @@ var SysEnv struct {
 	RunMod        int // 0 则为正常模式 ， 1 则为数据模拟模式
 }
 
+var BinanceKey = struct {
+	ApiKey    string `bson:"ApiKey"`
+	SecretKey string `bson:"SecretKey"`
+}{
+	ApiKey:    "e1ch7VN2DHqymTJMDRZLDjoyWHdjAaT2anp06elWJwOOZ51GoXXXQpOAeDenWEml",
+	SecretKey: "E9w2QmhtJC6Z326mB9fcCFH6syB2qt7TbvwaFfmxWtrWdAFrQNTIVvT8bSKLtV9e",
+}
+
 func LoadSysEnv(envPath string) {
 	viper.SetConfigFile(envPath)
 	err := viper.ReadInConfig()
