dc7b372a8a595a38cfa2aac6325629b4ed0bdd4a
墨七 <mo7@mo7.cc>
2023-02-05 00:19:13 +0800

    exit-push

--- _shell/Build.sh
+++ _shell/Build.sh
@@ -5,15 +5,16 @@ source "./_shell/init.sh"
 echo " =========== go build  =========== "
 
 go mod tidy &&
-  go build -o ${buildName}
-echo " server 端编译 完成"
+  go build -o "${BuildName}"
+
+echo " server 端编译 完成 "
 
 echo " =========== 开始进行文件整合 =========== "
-rm -rf ${outPutPath}
 
-mkdir ${outPutPath}
+rm -rf "${OutPutPath}"
+mkdir "${OutPutPath}"
 
 echo "移动 go build 文件"
-mv ${buildName} ${outPutPath}"/" &&
-  cp -r ${path}"/ReStart.sh" ${outPutPath}"/"
+mv "${BuildName}" "${OutPutPath}/" &&
+  cp -r "${NowPath}/ReStart.sh" "${OutPutPath}/"
 exit
