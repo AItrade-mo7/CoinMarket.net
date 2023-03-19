6a210e422e4d1427742e7c20b5ff4155687da890
墨七 <mo7@mo7.cc>
2023-03-20 02:48:02 +0800

    缩减体积

--- server/tmpl/Index.go
+++ server/tmpl/Index.go
@@ -4,15 +4,6 @@ import (
 	_ "embed"
 )
 
-//go:embed email-sys.html
-var SysEmail string
-
-type SysParam struct {
-	Message      string
-	SysTime      string
-	SecurityCode string
-}
-
 //go:embed ReClear.sh
 var ReClear string
 
