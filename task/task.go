276cd06502f83c56f40f90442d9938e10392d0a9
墨七 <mo7@mo7.cc>
2023-03-27 16:27:11 +0800

    x

--- task/task.go
+++ task/task.go
@@ -4,7 +4,8 @@ import (
 	_ "embed"
 
 	"CoinMarket.net/server/global"
-	"CoinMarket.net/task/dbTask"
+	"CoinMarket.net/task/testHunter"
+	"github.com/EasyGolang/goTools/mTime"
 )
 
 func main() {
@@ -12,14 +13,10 @@ func main() {
 	global.Start()
 
 	// 新建回测
-	// back := testHunter.New(testHunter.TestOpt{
-	// 	StartTime: mTime.TimeParse(mTime.Lay_ss, "2022-12-12T00:00:00"),
-	// 	EndTime:   mTime.TimeParse(mTime.Lay_ss, "2023-01-01T00:00:00"),
-	// 	InstID:    "BTC-USDT",
-	// })
-	// back.StuffDBKdata(func(KD mOKX.TypeKd) {
-	// 	global.Run.Println(KD.TimeStr, KD.C)
-	// })
-
-	dbTask.FormatKdata()
+	back := testHunter.New(testHunter.TestOpt{
+		StartTime: mTime.TimeParse(mTime.Lay_ss, "2022-12-12T00:00:00"),
+		EndTime:   mTime.TimeParse(mTime.Lay_ss, "2023-01-01T00:00:00"),
+		InstID:    "BTC-USDT",
+	})
+	back.StuffDBKdata()
 }
